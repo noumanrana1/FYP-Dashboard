d2e95bdae9d8f004e7b7e60c26f0000a89b66687
noumanrana1 <bsef18a016@pucit.edu.pk>
2022-06-01 01:26:22 +0500

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>{/* /.content-wrapper */}
+  <footer className="main-footer">
+    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div className="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+  {/* Control Sidebar */}
+  <aside className="control-sidebar control-sidebar-dark">
+    {/* Control sidebar content goes here */}
+  </aside>
+  {/* /.control-sidebar */}
+</div>
+
+    )
+  }
+}
